36d702c87bfb4bd81ea24bf4ec96610b1e2c4dd0
Chetan Bachchhav <156205330+chetanpatil45@users.noreply.github.com>
2026-03-14 17:11:32 +0530

    Update Assignment.java

--- Assignment.java
+++ Assignment.java
@@ -1,20 +1,24 @@
-public class Assignment{
+public class Assignment {
 
-  public static void main(String [] args){
-    int n = 5;
-    for (int i = 1; i <= n; i++) {
-       for (int j = i; j < n; j++) {
+    public static void main(String[] args) {
+
+        int n = 5;
+
+        for (int i = 1; i <= n; i++) {
+
+            for (int j = i; j < n; j++) {
                 System.out.print(" ");
-        }
+            }
 
-        for (int j = 1; j <= i; j++) {
-                System.out.print(j+" ");
-        }
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j);
+            }
 
-        for (int j = i - 1; j >= 1; j--) {
-                System.out.print(j+ " ");
+            for (int j = i - 1; j >= 1; j--) {
+                System.out.print(j);
+            }
+
+            System.out.println();
         }
-           System.out.println();
-        }
-  }
+    }
 }
